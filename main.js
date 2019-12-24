d5ac1a6be1c037be7a2c2e5be54a7542f826144b
miho yamagata <miho.yamagata@gmail.com>
2019-12-24 23:16:51 +0900

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+function getAction() {
+    // playingクラスを追加
+    const getClass = document.getElementsByClassName('key');
+    for (let key of getClass) {
+        key.classList.add('playing');　　　
+    }　
 
+}
+
+getAction();
+
+    // dataを取得
+    const keyAction = document.querySelectorAll('[data-key="playing"]');
+    console.log(keyAction.dataset);
+
+
+
+
+
+
+// // playingクラスを削除
+// function classRemove() {
+//     const rmClass = document.getElementsByClassName('key');
+//     for (let key of rmClass) {
+//         key.classList.remove('playing');
+//     }
+// }
+
+// classRemove();

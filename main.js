5238130d935b7c8324ba037b26a9dc7b982dd732
miho yamagata <miho.yamagata@gmail.com>
2020-01-02 15:45:20 +0900

    submit

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
-// playingクラスを追加
-function getAction() {
-    const getClass = document.getElementsByClassName('key');
-    for (let key of getClass) {
-        key.classList.add('playing');　　　
-    }　
 
-}
-
-window.addEventListener('keydown', getAction);
-
-// dataを取得
 function keycodeOutput(e) {
     const keyAction = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const audioAction = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    keyAction.classList.add('playing');
+    audioAction.play();
+    
     console.log(keyAction);
     console.log(audioAction);
-    
-    function playSound() {
-        audio.play();
-    }
-}
+
+ }
 
 window.addEventListener('keydown', keycodeOutput);
 
 
+function keycodeRemove(e) {
+    const keyAction = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audioAction = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
+    keyAction.classList.remove('playing');
+    audioAction.pause();
 
-// // playingクラスを削除
-// function classRemove() {
-//     const rmClass = document.getElementsByClassName('key');
-//     for (let key of rmClass) {
-//         key.classList.remove('playing');
-//     }
-// }
+    console.log(keyAction);
+    console.log(audioAction);
 
-// classRemove();
+}
+
+window.addEventListener('keyup', keycodeRemove);
